b12aaeea067ad8f76e4fa310b3997b7cc8bd2558
Carina Cruz <lecruz@bu.edu>
2018-03-26 02:58:40 +0000

    section 5 commits

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 require('./api/data/db.js');
 var express = require('express');
+require('./api/data/db.js');
+var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
--- public/angular_app/hotel-list/hotel-list-controller.js
+++ public/angular_app/hotel-list/hotel-list-controller.js
@@ -1,3 +1,5 @@
+/*global angular*/
+
 angular.module('meanhotel').controller('HotelsController', HotelsController);
 
 function HotelsController(hotelDataFactory) {
